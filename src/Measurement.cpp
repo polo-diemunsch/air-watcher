94bab56b0c39fb0d045e551774384c0998c0e4dd
Polo <polo.diemunsch@gmail.com>
2024-05-05 01:30:00 +0200

    feat(Measurement): sensor measurement class

---
+++ src/Measurement.cpp
@@ -0,0 +1,81 @@
+/*************************************************************************
+                           Measurement  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation de la classe <Measurement> (fichier Measurement.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Measurement.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+time_t Measurement::GetTimestamp ( ) const
+// Algorithme :
+//
+{
+    return timestamp;
+} //----- Fin de GetTimestamp
+
+Attribute Measurement::GetAttribute ( ) const
+// Algorithme :
+//
+{
+    return *attribute;
+} //----- Fin de GetAttribute
+
+double Measurement::GetValue ( ) const
+// Algorithme :
+//
+{
+    return value;
+} //----- Fin de GetValue
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+ostream & operator << (ostream & out, const Measurement & measurement)
+// Algorithme :
+//
+{
+    out << "Measurement(timestamp: " << measurement.timestamp << ", attribute: " << measurement.attribute << ", value: " << measurement.value << ")";
+    return out;
+} //----- Fin de operator <<
+
+
+//-------------------------------------------- Constructeurs - destructeur
+Measurement::Measurement ( const time_t timestamp, const Attribute * attributePointer, const double value ) :
+timestamp(timestamp), attribute(attributePointer), value(value)
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Measurement>" << endl;
+#endif
+} //----- Fin de Measurement
+
+
+Measurement::~Measurement ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Measurement>" << endl;
+#endif
+} //----- Fin de ~Measurement
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
---
+++ src/Measurement.h
@@ -0,0 +1,87 @@
+/*************************************************************************
+                           Measurement  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <Measurement> (fichier Measurement.h) ----------------
+#if ! defined ( MEASUREMENT_H )
+#define MEASUREMENT_H
+
+//--------------------------------------------------- Interfaces utilisées
+using namespace std;
+#include <string>
+#include <ctime>
+
+#include "Attribute.h"
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Measurement>
+//
+//
+//------------------------------------------------------------------------
+
+class Measurement
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    time_t GetTimestamp ( ) const;
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    Attribute GetAttribute ( ) const;
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    double GetValue ( ) const;
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------- Surcharge d'opérateurs
+    friend ostream & operator << (ostream & out, const Measurement & measurement);
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//-------------------------------------------- Constructeurs - destructeur
+    Measurement ( const time_t timestamp, const Attribute * attribute, const double value );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~Measurement ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+    const time_t timestamp;
+    const Attribute * attribute;
+    const double value;
+};
+
+//-------------------------------- Autres définitions dépendantes de <Measurement>
+
+#endif // MEASUREMENT_H
