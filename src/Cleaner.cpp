3781c44bcb93037cd88f00c051804b13ab2b3d9e
Polo <polo.diemunsch@gmail.com>
2024-05-05 19:28:24 +0200

    feat(Cleaner): air cleaner class

---
+++ src/Cleaner.cpp
@@ -0,0 +1,120 @@
+/*************************************************************************
+                           Cleaner  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation de la classe <Cleaner> (fichier Cleaner.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Cleaner.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+string Cleaner::GetId ( ) const
+// Algorithme :
+//
+{
+    return id;
+} //----- Fin de GetId
+
+double Cleaner::GetLatitude ( ) const
+// Algorithme :
+//
+{
+    return latitude;
+} //----- Fin de GetLatitude
+
+double Cleaner::GetLongitude ( ) const
+// Algorithme :
+//
+{
+    return longitude;
+} //----- Fin de GetLongitude
+
+pair<double, double> Cleaner::GetPosition ( ) const
+// Algorithme :
+//
+{
+    return make_pair(latitude, longitude);
+} //----- Fin de GetLongitude
+
+time_t Cleaner::GetStartDate ( ) const
+// Algorithme :
+//
+{
+    return startDate;
+} //----- Fin de GetStartDate
+
+time_t Cleaner::GetEndDate ( ) const
+// Algorithme :
+//
+{
+    return endDate;
+} //----- Fin de GetEndDate
+
+const Provider * Cleaner::GetProvider ( ) const
+// Algorithme :
+//
+{
+    return provider;
+} //----- Fin de GetProvider
+
+void Cleaner::SetProvider ( const Provider * providerPointer )
+// Algorithme :
+//
+{
+    provider = providerPointer;
+} //----- Fin de SetProvider
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+ostream & operator << (ostream & out, const Cleaner & cleaner)
+// Algorithme :
+//
+{
+    out << "Cleaner(id: " << cleaner.id<< ", latitude: " << cleaner.latitude << ", longitude: " << cleaner.longitude
+        << ", startDate: " << cleaner.startDate << ", endDate: " << cleaner.endDate             // TODO format time_t (see measurement too)
+        << ")";
+    return out;
+} //----- Fin de operator <<
+
+
+//-------------------------------------------- Constructeurs - destructeur
+Cleaner::Cleaner ( const string & id, const double latitude, const double longitude,
+                   const time_t startDate, const time_t endDate ) :
+id(id), latitude(latitude), longitude(longitude), startDate(startDate), endDate(endDate)
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Cleaner>" << endl;
+#endif
+    provider = nullptr;
+} //----- Fin de Cleaner
+
+
+Cleaner::~Cleaner ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Cleaner>" << endl;
+#endif
+} //----- Fin de ~Cleaner
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
---
+++ src/Cleaner.h
@@ -0,0 +1,126 @@
+/*************************************************************************
+                           Cleaner  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <Cleaner> (fichier Cleaner.h) ----------------
+#if ! defined ( CLEANER_H )
+#define CLEANER_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include <string>
+#include <ctime>
+
+class Provider;
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Cleaner>
+//
+//
+//------------------------------------------------------------------------
+
+class Cleaner
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    string GetId ( ) const;
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    double GetLatitude ( ) const;
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    double GetLongitude ( ) const;
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    pair<double, double> GetPosition ( ) const;
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    time_t GetStartDate ( ) const;
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    time_t GetEndDate ( ) const;
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    bool IsActiveAt ( const time_t date ) const;
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    const Provider * GetProvider ( ) const;
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    void SetProvider ( const Provider * provider );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------- Surcharge d'opérateurs
+    friend ostream & operator << (ostream & out, const Cleaner & cleaner);
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//-------------------------------------------- Constructeurs - destructeur
+    Cleaner ( const string & id, const double latitude, const double longitude,
+              const time_t startDate, const time_t endDate );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~Cleaner ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+    const string id;
+    const double latitude;
+    const double longitude;
+    const time_t startDate;
+    const time_t endDate;
+    const Provider * provider;
+};
+
+//-------------------------------- Autres définitions dépendantes de <Cleaner>
+
+#endif // CLEANER_H
